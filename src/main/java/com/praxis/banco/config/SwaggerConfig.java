6e91b9554864a5bb2f2b20efd79e3f28a7230a60
Ulises Gaspar Bautista <gabu@praxisit.local>
2020-02-07 12:51:51 -0600

    Se actualiza servicios

---
+++ src/main/java/com/praxis/banco/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package com.praxis.banco.config;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**  
+  * Santander Mexico<br>  
+  * <br><b>Project:</b> li-perfilamiento-service  
+  * <br><b>Class:</b> SwaggerConfig.java  
+  * <br><b>Description:</b> Configuracion para habilitar swagger  
+  *  
+  * @author Eduardo Castillo Mendoza  
+  * @company Praxis  
+  * @created 1 ago. 2019  
+  * @since JDK1.8  
+  *  
+  * @version Control de cambios:  
+  * @version 1.0 1 ago. 2019 FSW Lacertus Nombre del autor:   
+  * Descripcion breve del cambio  
+  *  
+  * @category Incluir si la clase es un Modelo, Service, etc.  
+  *     
+  */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements Serializable {
+
+	/**
+	 * serial version uid.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Bean para scannear las APIs existentes en el proyecto y generar el
+	 * swagger-ui.
+	 * 
+	 * @return docket con configuracion para scannar APIs.
+	 */
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(Boolean.FALSE).select()
+				.apis(RequestHandlerSelectors.basePackage("com.praxis.banco.controler"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	/**
+	 * Builder para el ApiInfo.
+	 * 
+	 * @return apiInfo con la informacion de la construccion del API.
+	 */
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Clientes Service").description("CRUD de Clientes").version("1.0")
+				.contact(new Contact("Ulises Gaspar Bautista", " ", "gabu@praxis.com.mx")).build();
+	}
+
+}
---
+++ src/main/java/com/praxis/banco/domain/StoreSport.java
@@ -0,0 +1,33 @@
+package com.praxis.banco.domain;
+
+public class StoreSport {
+
+	private Long idProducto;
+	private String nombreProducto;
+	private String descripcion;
+
+	public Long getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(Long idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
